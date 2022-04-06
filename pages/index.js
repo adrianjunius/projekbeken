494209ceaa7cca92a403f70a7c41849834393c29
Gregorius21 <72364117+Gregorius21@users.noreply.github.com>
2022-04-06 12:40:29 +0700

    projek beken

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import { useWeb3 } from '@3rdweb/hooks'
 import { useEffect } from 'react'
-import { client } from '../lib/sanityClient'
 import toast, { Toaster } from 'react-hot-toast'
+import sanityClient from '@sanity/client'
 
 const style = {
   wrapper: ``,
@@ -15,35 +15,6 @@ const style = {
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
-
-  const welcomeUser = (userName, toastHandler = toast) => {
-    toastHandler.success(
-      `Welcome back${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
-      {
-        style: {
-          background: '#04111d',
-          color: '#fff',
-        },
-      }
-    )
-  }
-
-  useEffect(() => {
-    if (!address) return
-    ;(async () => {
-      const userDoc = {
-        _type: 'users',
-        _id: address,
-        userName: 'Unnamed',
-        walletAddress: address,
-      }
-
-      const result = await client.createIfNotExists(userDoc)
-
-      welcomeUser(result.userName)
-    })()
-  }, [address])
-
   return (
     <div className={style.wrapper}>
       <Toaster position="top-center" reverseOrder={false} />
